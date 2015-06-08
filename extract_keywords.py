7d3cc3affad4c325cc38ab2054bd6d6732f8e9a5
leeopop <dlrmsghd@gmail.com>
2015-06-08 23:22:55 +0900

    extract keywords

---
+++ extract_keywords.py
@@ -0,0 +1,60 @@
+import csv
+import re
+from loader import *
+from collections import defaultdict
+
+def split_line(single_string):
+	keyword_stop = ['key', 'word']
+	keyword_split = r'[\,\.\'\"\:\; \`\|\(\)]'
+	splitter = re.compile(keyword_split, flags=re.UNICODE)
+
+	single_string = single_string.lower()
+	words = splitter.split(single_string)
+	words = [x.strip() for x in words]
+	return words
+
+def create_keyword_table(paper_data, word_limit=30):
+	word_count = defaultdict(int)
+	for (id,field) in paper_data.items():
+		title = field['title']
+		title_words = split_line(title)
+
+		keyword_list = field['keyword']
+		key_words = split_line(keyword_list)
+
+		for word in title_words:
+			word_count[word] += 1
+
+		for word in key_words:
+			word_count[word] += 1
+
+	keyword_table = dict()
+	list_word = [x for x in word_count.items()]
+	list_word = sorted(list_word, key=lambda x: x[0]) # use 1 to sort by frequency
+
+
+	for (unique_id, item) in zip( range(len(list_word)), list_word):
+		(word, count) = item
+		if(count < word_limit):
+			continue
+		keyword_table[word] = unique_id
+	return keyword_table
+
+
+def save_keyword_table(keyword_table, output_file):
+	with open(output_file, 'w', encoding='utf-8') as write_file:
+		writer = csv.writer(write_file)
+		writer.writerow(['id','unique'])
+
+		for key in sorted(keyword_table.keys()):
+			writer.writerow([key, keyword_table[key]])
+
+
+def main():
+	paper_data = load_single_file('Paper.csv')
+	keyword_table = create_keyword_table(paper_data)
+	save_keyword_table(keyword_table, 'keyword_table.csv')
+	pass
+
+if __name__ == '__main__':
+	main()
