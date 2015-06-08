0ae38c442dad1fa1284d21d2c38616740d137550
leeopop <dlrmsghd@gmail.com>
2015-06-08 17:44:49 +0900

    cv2->scikit

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-import cv2
-import cv2.ocl
-import cv2.ml
 import csv
 import numpy
-import sys
+import sklearn
+import sklearn.ensemble
+import sklearn.ensemble.forest
+import multiprocessing
 
-def enableOpenCL(verbose = False):
-	has_opencl = cv2.ocl.haveOpenCL()
-	if(verbose):
-		print("Have openCL? {0}".format(has_opencl))
-	if has_opencl:
-		return cv2.ocl.useOpenCL()
-	else:
-		return False
-
-def read_data(filename, result_type = cv2.ml.VAR_CATEGORICAL):
+#is_class
+def read_data(filename, classification=True):
 	with open(filename, 'r', encoding='utf-8') as read_file:
 		samples = []
 		responses = []
@@ -22,16 +14,16 @@ def read_data(filename, result_type = cv2.ml.VAR_CATEGORICAL):
 		column = reader.__next__()
 
 		target_id = -1
-		var_type = []
+		is_class = []
 		for i in range(len(column)):
 			if column[i].lower() == "target":
 				target_id = i
-				#var_type.append(cv2.ml.VAR_CATEGORICAL)
+				#is_class.append(cv2.ml.VAR_CATEGORICAL)
 			elif 'has_' in column[i] or 'type_' in column[i]:
-				var_type.append(cv2.ml.VAR_CATEGORICAL)
+				is_class.append(True)
 			else:
-				var_type.append(cv2.ml.VAR_NUMERICAL)
-		var_type.append(result_type)
+				is_class.append(False)
+		#is_class.append(result_type)
 
 		for line in reader:
 			if len(line) != len(column):
@@ -48,107 +40,85 @@ def read_data(filename, result_type = cv2.ml.VAR_CATEGORICAL):
 				sample_line.append(x)
 			samples.append(sample_line)
 			responses.append(response_line)
-		result_dtype = numpy.int32
-		if(result_type == cv2.ml.VAR_NUMERICAL):
-			result_dtype = numpy.float32
+		result_dtype = numpy.float32
+		if(classification == True):
+			result_dtype = numpy.int32
 		return (numpy.array(samples, dtype=numpy.float32),
 		        numpy.array(responses, dtype=result_dtype),
-		        numpy.array(var_type, dtype=numpy.int32))
+		        numpy.array(is_class, dtype=numpy.int32))
 
-def categorize_data(matrix, responses, var_type, K, center_dict = None):
-	todo_list = []
-	for (i,type) in zip(range(len(var_type)), var_type):
-		if type == cv2.ml.VAR_NUMERICAL:
-			todo_list.append(i)
-	#ret_mat = matrix.astype(numpy.int32, copy=True)
-	print(todo_list)
-	do_kmean = False
-	if center_dict == None:
-		do_kmean = True
-		center_dict = {}
-	for i in todo_list:
-		col = matrix[:,i]
-		#print(matrix[:,i])
-		if do_kmean:
-			_temp = []
-			for _i in range(len(col)):
-				_temp.append([0])
-			best = numpy.array(_temp)
-			(error, matching, center) = cv2.kmeans(data=col, K=K, bestLabels=best,
-			                    criteria=(cv2.TermCriteria_MAX_ITER,len(col), 0.0001),
-			                    attempts=1, flags=cv2.KMEANS_PP_CENTERS)
-			for _i in range(len(col)):
-				matrix[_i,i] = best[_i]
-			center_dict[i] = center
-		else:
-			centers = center_dict[i]
-			for _i in range(len(col)):
-				error = float('inf')
-				prev_val = matrix[_i,i]
-				index = (numpy.abs(centers-prev_val)).argmin()
-				matrix[_i,i] = index
+def read_index(filename):
+	mapping = []
+	with open(filename, 'r', encoding='utf-8') as read_file:
+		reader = csv.reader(read_file)
+		column = reader.__next__()
+		author_id = -1
+		paper_id = -1
 
-		#print(matrix[:,i])
-		var_type[i] = cv2.ml.VAR_CATEGORICAL
-	print(matrix)
-	return (matrix, responses, var_type, center_dict)
+		for i in range(len(column)):
+			if column[i].lower().strip() == "authorid":
+				author_id = i
+				#is_class.append(cv2.ml.VAR_CATEGORICAL)
+			elif column[i].lower().strip() == "paperid":
+				paper_id = i
+
+		for line in reader:
+			if len(line) != len(column):
+				continue
+			author = None
+			paper = None
+			for (x,i) in zip(line, range(len(line))):
+				if i == author_id:
+					author = int(x)
+				elif i == paper_id:
+					paper = int(x)
+			mapping.append(tuple(author,paper))
+	return mapping
+
+def submit(input_file, output_file, score_dict):
+	with open(input_file, "r", encoding='utf-8') as read_file:
+		with open(output_file, "w", encoding='utf-8') as write_file:
+			reader = csv.reader(read_file)
+			column = reader.__next__()
+			assert(len(column) == 2)
+
+			writer = csv.writer(write_file)
+			writer.writerow(column)
+
+			for [author,papers] in reader:
+				list_papers = papers.strip().split()
+				sorted(list_papers, key=lambda paper: score_dict[(int(author), int(paper))][1])
+
+				writer.writerow([author, ' '.join(list_papers)])
+
+
 
 def main():
+	(samples,responses,is_class) = read_data("train_data.csv")
+	print("Using {} CPUs".format(multiprocessing.cpu_count()))
+	classifier = sklearn.ensemble.RandomForestClassifier(
+		n_estimators = 1500,
+		#criterion = 'entropy',
+		max_features = 'auto',
+		max_depth = 5,
+		n_jobs = multiprocessing.cpu_count(),
+		verbose = 1
+	)
+	classifier.fit(samples, responses)
+	ret = classifier.score(samples, responses)
+	print("Train score: {}",format(ret))
 
-	result_type = cv2.ml.VAR_CATEGORICAL
-	print(enableOpenCL(verbose = True))
-	(samples,responses,var_type) = read_data('train_data.csv', result_type=result_type)
-	#(samples,responses,var_type, center_dict) = categorize_data(samples,responses,var_type, 10)
+	(tests,responses,is_class) = read_data("test_data.csv")
+	predict_result = classifier.predict_proba(tests)
+	label = read_index("test_index.csv")
+	print("result_len: {}, label_len: {}".format(len(predict_result), len(label)))
 
+	score_dict = dict()
+	for (prob, key) in zip(predict_result, label):
+		assert(not key in score_dict.keys())
+		score_dict[key] = prob
 
-	print(var_type)
-	train_data = cv2.ml.TrainData_create(samples, cv2.ml.ROW_SAMPLE, responses)
-	print(train_data)
-
-
-	train_model = cv2.ml.Boost_create()
-
-	train_model.setBoostType(cv2.ml.BOOST_REAL)
-	train_model.setWeightTrimRate(0.999)
-	train_model.setRegressionAccuracy(0.999)
-
-	train_model.setMaxCategories(1500)
-	train_model.setMaxDepth(5)
-
-	ret = train_model.train(train_data)
-	print(ret)
-	print('isClassifier? {}'.format(train_model.isClassifier()))
-	print('Boost model: {}'.format(train_model.getBoostType()))
-	print('Weak count: {}'.format(train_model.getWeakCount()))
-	print('Weight trim rate: {}'.format(train_model.getWeightTrimRate()))
-	print('Variable count: {}'.format(train_model.getVarCount()))
-
-	#file_storage = cv2.FileStorage('train_data', flags=cv2.FileStorage_WRITE, encoding='utf-8')
-	#print(file_storage)
-	#file_storage.writeObj('train_model', train_model)
-
-#def main2():
-	#file_storage = cv2.FileStorage('train_data', flags=cv2.FileStorage_READ, encoding='utf-8')
-	#train_model = file_storage.readObj()
-	print(train_model)
-	(tests,responses, var_type) = read_data('test_data.csv', result_type=result_type)
-	#(tests,responses,var_type, center_dict) = categorize_data(samples,responses,var_type, 10, center_dict=center_dict)
-	print(var_type)
-
-	err = train_model.predict(tests)
-	print("Error: {}".format(err[0]))
-	print(err[1])
-	print(len(err[1]))
-
-	return
-	for test in tests:
-		x = [test.tolist()]
-		print (x)
-		err = train_model.predict(test)
-		print("Error: {}".format(err[0]))
-		print(err[1])
-	#for x in err[1]:
-	#	print(x)
+	submit("Test.csv", "submit.csv", score_dict)
 
 if __name__ == '__main__':
 	main()
